bf93ae70b69e5c2bd75a9a0d0a1ed4d7af37f9a7
Will <megapocketninja@gmail.com>
2019-04-04 14:00:07 -0400

    Gameboard initilization and logic for click of each space

--- t3.js
+++ t3.js
@@ -7,8 +7,7 @@ class Space {
     }
     markSpace(player) {
         this.mark = player
-        console.log("clicked")
-        document.querySelector("#space" + this.id).removeEventListener("click", this.markSpace)
+        console.log("clicked" + this.id)
     }
 }
 
@@ -20,19 +19,29 @@ const gameBoard = {
         row1: [],
         row2: []
     },
-    makeButtons: function () {
-        for (let i = 0; i < 3; i++) {
-            let tempSpace = 0
-            for (let j = 1; j < 4; j++) {
-                tempSpace = new Space(`${i}` + `${j}`)
-                row = "row" + i
-                this.spaces[`${row}`].push(tempSpace)
-                document.querySelector("#space" + i + j).addEventListener("click", () => {
-                    this.spaces[row][j - 1].markSpace(this.player)
-                })
+    makeBoard: function () {
+        this.makeRow("0")
+        this.makeRow("1")
+        this.makeRow("2")
+    },
+    makeRow: function (rowNum) {
+        for (let i = 1; i < 4; i++) {
+            //makes a new Space object
+            let tempSpace = new Space(rowNum + `${i}`)
+            let clear = function () { }
+            const mark = function () {
+                tempSpace.markSpace(this.player)
+                clear()
             }
+            //assigns a function to clear that runs after the listner is triggered and removes the listener so space can only be clicked once.
+            clear = function () {
+                document.querySelector("#space" + tempSpace.id).removeEventListener("click", mark, true)
+            }
+            document.querySelector("#space" + tempSpace.id).addEventListener("click", mark, true)
+            this.spaces["row" + rowNum].push(tempSpace)
+
         }
     }
-
-
 }
+
+gameBoard.makeBoard()
